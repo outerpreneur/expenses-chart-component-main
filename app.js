a88513c55501b0f765cc9b454c2e1c09c04404d1
Alek Tysklind <91225228+outerpreneur@users.noreply.github.com>
2023-02-01 13:50:01 +0100

    javascript events added

--- app.js
+++ app.js
@@ -10,13 +10,13 @@
 data.forEach(day => {
     let chartDiv = document.createElement('div');
     chartDiv.classList.add(counter++);
-    console.log(counter)
 
     let percentageElement = document.createElement("span");
     percentageElement.classList.add("percentage-data");
     percentageElement.textContent = `${day.amount}`;
     percentageElement.style.visibility = "hidden";
     chartDiv.append(percentageElement);
+    
 
     let chartData = document.createElement('span');
     chartData.classList.add("chart-data");
@@ -31,6 +31,37 @@ data.forEach(day => {
     chart.append(chartDiv);
 
 });
+
+// adding a variable for all the children elements within the chart
+const chartNodes = chart.childNodes
+// show the chart active color depending on day of the week
+for (let i = 0; i < chartNodes.length; i++) {
+    if (dayOfWeek == chartNodes[i].classList) {
+        chartNodes[i].children[1].classList.add("active")
+        break
+    }
+}
+
+// add variable name to new created element= chartData
+const chartData = document.querySelectorAll(".chart-data")
+// changing visibility so it shows percentage divs on hover
+chart.addEventListener('mouseover', e => {
+    for (let i = 0; i < chartData.length; i++ ) {
+        if (e.target == chartData[i]) {
+            chartData[i].previousSibling.style.visibility = "visible"
+        }
+    }
+})
+
+chart.addEventListener("mouseout", e => {
+    for( let i = 0; i < chartData.length; i++ ) {
+        if(e.target == chartData[i]) {
+            chartData[i].previousSibling.style.visibility = "hidden"
+        }
+    }
+})
+
+
 
 
 })();
